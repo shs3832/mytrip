cc5129b261aa53b488d4d32a32f926019ba3038d
Mangomeee_ <shs3832@naver.com>
2026-05-18 11:52:27 +0900

    homework9 게시글 등록 및 조회 기능 구현

---
+++ src/app/homework9/boards/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useRouter } from "next/navigation";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      createdAt
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
+export default function BoardListPage() {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARDS, {
+    variables: {
+      page: 1,
+    },
+  });
+  console.log(data);
+  const handleViewDetail = (id) => {
+    router.push(`boards/${id}`);
+  };
+
+  const [delete_board] = useMutation(DELETE_BOARD);
+
+  const handleDelete = async (id) => {
+    const result = await delete_board({
+      variables: {
+        boardId: id,
+      },
+      refetchQueries: [FETCH_BOARDS],
+    });
+    console.log(result);
+    alert("삭제되었습니다");
+  };
+
+  return (
+    <>
+      <div className="shadow-md rounded-3xl py-6 px-12 ">
+        <div className="w-full text-center ">
+          <div>
+            <div className="table-title flex items-center w-full font-medium text-base text-gray-900">
+              <span className="px-6 py-3 w-[100px] shrink-0">번호</span>
+              <span className="px-6 py-3 grow text-left">제목</span>
+              <span className="px-6 py-3 w-1/6">작성자</span>
+              <span className="px-6 py-3 w-1/6">날짜</span>
+              <span className="px-6 py-3 w-20">&nbsp;</span>
+            </div>
+          </div>
+          <div>
+            {data?.fetchBoards?.map((el, index) => {
+              return (
+                <div
+                  key={`el` + index}
+                  className="group flex items-center w-full my-2 rounded-md border border-gray-100 hover:bg-gray-50 cursor-pointer"
+                >
+                  <span className="px-6 py-3 text-gray-500 font-light w-[100px] shrink-0">
+                    {index + 1}
+                  </span>
+                  <span
+                    className="px-6 py-3 grow text-left font-medium text-gray-900"
+                    onClick={() => {
+                      handleViewDetail(el._id);
+                    }}
+                  >
+                    {el.title}
+                  </span>
+                  <span className="px-6 py-3 w-1/6">{el.writer}</span>
+                  <span className="px-6 py-3 w-1/6 text-gray-500">
+                    {new Date(el.createdAt).toISOString().slice(0, 10)}
+                  </span>
+                  <span className="px-6 py-3 w-20">
+                    <span
+                      onClick={() => {
+                        handleDelete(el._id);
+                      }}
+                      className="invisible inline-block group-hover:visible"
+                    >
+                      삭제
+                    </span>
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
